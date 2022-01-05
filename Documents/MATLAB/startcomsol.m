4d8c6a0d208f8a7fb8d0f6867326501331bcc2af
Christopher Nies <chris-nies@hotmail.de>
2022-01-05 14:59:14 +0100

    MATALB startup, COMSOL server

---
+++ Documents/MATLAB/startcomsol.m
@@ -0,0 +1,5 @@
+workingDir = pwd;
+cd /usr/local/comsol55/multiphysics/mli/
+mphstart;
+cd(workingDir);
+clear workingDir
---
+++ Documents/MATLAB/startup.m
@@ -0,0 +1,10 @@
+fullpath = genpath('/home/chris/Documents/MATLAB');
+addpath(fullpath);
+% rm folders with redundant functions
+rmpath(genpath('/home/chris/Documents/MATLAB/unitcell/gmsh_tests/'));
+rmpath(genpath('/home/chris/Documents/MATLAB/unitcell/minimal-example-main/'));
+rmpath(genpath('/home/chris/Documents/MATLAB/unitcell/tests/'));
+rmpath(genpath('/home/chris/Documents/MATLAB/ferreira_matlab/'));
+
+cd ~/Documents/MATLAB/unitcell/
+clear fullpath
