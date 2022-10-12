4642c25c38110c4db0071a24c5736eed39bda5dc
Christopher Nies <chris-nies@hotmail.de>
2022-10-12 08:10:04 +0200

    system generated

--- Documents/MATLAB/startup.m
+++ Documents/MATLAB/startup.m
@@ -1,10 +1,9 @@
-fullpath = genpath('/home/chris/Documents/MATLAB');
+fullpath = genpath('/home/chris/Documents/MATLAB/');
 addpath(fullpath);
 % rm folders with redundant functions
-rmpath(genpath('/home/chris/Documents/MATLAB/unitcell/gmsh_tests/'));
-rmpath(genpath('/home/chris/Documents/MATLAB/unitcell/minimal-example-main/'));
-rmpath(genpath('/home/chris/Documents/MATLAB/unitcell/tests/'));
+rmpath(genpath('/home/chris/Documents/MATLAB/unitcell/'));
 rmpath(genpath('/home/chris/Documents/MATLAB/ferreira_matlab/'));
+rmpath(genpath('/home/chris/Documents/MATLAB/femlab/'));
 
-cd ~/Documents/MATLAB/unitcell/ComplexBandstructure
+cd ~/Documents/MATLAB/hexUnitCell
 clear fullpath
